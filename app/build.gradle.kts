2aa728680b5843bbd4c349d9d6079103264dc4d1
sameerasw <sameerasw.com@gmail.com>
2026-07-02 18:35:56 +0530

    chore: bump version code to 3 and version name to 3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.sameerasw.essentials"
         minSdk = 35
         targetSdk = 36
-        versionCode = 2
-        versionName = "2.0"
+        versionCode = 3
+        versionName = "3.0"
 
     }
 
